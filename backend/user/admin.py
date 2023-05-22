047692b363fb000578cc20b77dd3d3a68a50fe30
Nikita Korovyakovskiy@mail.ru <nikita.korovyakovskiy@mail.ru>
2023-05-22 18:57:37 +0700

    22.05

--- backend/user/admin.py
+++ backend/user/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+
 from user.models import Subscribe, User
 
 
 class UserADmin(UserAdmin):
-    # list_display = ("id", "username", "first_name", "last_name", "email")
-    # list_filter = ("first_name", "email")
-    list_display = ["id"]
+    list_display = ("id", "username", "first_name", "last_name", "email")
+    list_filter = ("first_name", "email")
 
 
 class SubscribeADmin(admin.ModelAdmin):
