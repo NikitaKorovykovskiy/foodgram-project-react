28f9a027bfa0efa808d740c06fa58c8f877b0499
Nikita Korovyakovskiy@mail.ru <nikita.korovyakovskiy@mail.ru>
2023-05-23 20:11:28 +0700

    23.05

--- backend/api/views.py
+++ backend/api/views.py
@@ -41,12 +41,12 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        serializer = RecipeGetSerializer(instance=serializer.instance)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    # def create(self, request, *args, **kwargs):
+    #     serializer = self.get_serializer(data=request.data)
+    #     serializer.is_valid(raise_exception=True)
+    #     self.perform_create(serializer)
+    #     serializer = RecipeGetSerializer(instance=serializer.instance)
+    #     return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def update(self, request, *args, **kwargs):
         partial = kwargs.pop("partial", False)
