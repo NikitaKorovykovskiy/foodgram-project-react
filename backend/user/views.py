c85ca1e663af33a1014f5ab5cc3da1eaae80c6f7
Nikita Korovyakovskiy@mail.ru <nikita.korovyakovskiy@mail.ru>
2023-05-23 18:34:02 +0700

    23.05

--- backend/user/views.py
+++ backend/user/views.py
@@ -5,7 +5,7 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from api.paginators import LimitPageNumberPagination
-from recipes.models import User
+from user.models import User
 from user.models import Subscribe
 from user.serializers import SubscribeShowSerializer
 
