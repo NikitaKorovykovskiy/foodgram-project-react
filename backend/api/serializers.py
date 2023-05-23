a710d302c4c6a98b697763d4be1bf5a435445ada
Nikita Korovyakovskiy@mail.ru <nikita.korovyakovskiy@mail.ru>
2023-05-23 20:53:48 +0700

    23.05 вернул создание рецепта

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404
 from drf_extra_fields.fields import Base64ImageField
-from rest_framework import exceptions, serializers
+from rest_framework import serializers
 from rest_framework.validators import (
     UniqueTogetherValidator,
     UniqueValidator,
