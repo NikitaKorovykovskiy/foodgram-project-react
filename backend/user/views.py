2a7d5a1ba6962711cda2889f697a8c029eb9bc5a
Nikita Korovyakovskiy@mail.ru <nikita.korovyakovskiy@mail.ru>
2023-05-27 16:31:55 +0700

    После сдачи

--- backend/user/views.py
+++ backend/user/views.py
@@ -1,12 +1,17 @@
+from api.paginators import LimitPageNumberPagination
+from api.permissions import IsAdminOrReadOnly
 from django.shortcuts import get_object_or_404
 from djoser.views import UserViewSet
-
-from rest_framework import permissions, status
+from rest_framework import permissions
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.status import (
+    HTTP_201_CREATED,
+    HTTP_204_NO_CONTENT,
+    HTTP_400_BAD_REQUEST,
+)
 from user.models import Subscribe, User
 from user.serializers import SubscribeShowSerializer
-from api.paginators import LimitPageNumberPagination
 
 
 class SubscribeUserViewSet(UserViewSet):
@@ -16,46 +21,8 @@ class SubscribeUserViewSet(UserViewSet):
     pagination_class = LimitPageNumberPagination
 
     @action(
-        detail=True,
-        methods=("POST", "DELETE"),
-        url_path="subscribe",
-        permission_classes=[permissions.IsAuthenticatedOrReadOnly],
-    )
-    def subscribe(self, request, id=None):
-        user = get_object_or_404(User, id=id)
-        follow = Subscribe.objects.filter(
-            user=request.user, following=user
-        )
-        if request.method == "POST":
-            if user == request.user:
-                error = {"errors": "Вы пытаетесь подписаться на себя."}
-                return Response(error, status=status.HTTP_400_BAD_REQUEST)
-            obj, created = Subscribe.objects.get_or_create(
-                user=request.user, following=user
-            )
-            if not created:
-                error = {
-                    "errors": "Вы уже подписаны на этого пользователя."
-                }
-                return Response(error, status=status.HTTP_400_BAD_REQUEST)
-            serializer = SubscribeShowSerializer(
-                obj, context={"request": request}
-            )
-            return Response(
-                serializer.data, status=status.HTTP_201_CREATED
-            )
-
-        if not follow.exists():
-            error = {
-                "errors": "Вы не были подписаны на этого пользователя."
-            }
-            return Response(error, status=status.HTTP_400_BAD_REQUEST)
-        follow.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-    @action(
         detail=False,
-        permission_classes=[permissions.IsAuthenticatedOrReadOnly],
+        permission_classes=(permissions.IsAuthenticatedOrReadOnly,),
     )
     def subscriptions(self, request):
         follows = User.objects.filter(following__user=request.user)
@@ -64,3 +31,33 @@ class SubscribeUserViewSet(UserViewSet):
             pages, many=True, context={"request": request}
         )
         return self.get_paginated_response(serializer.data)
+
+    @action(
+        methods=["post"],
+        detail=True,
+        permission_classes=(IsAdminOrReadOnly,),
+    )
+    def subscribe(self, request, **kwargs):
+        author = get_object_or_404(User, id=self.kwargs.get("id"))
+        serializer = SubscribeShowSerializer(
+            author, data=request.data, context={"request": request}
+        )
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+        data = {"author": author}
+        serializer.create(data)
+        serializer.save()
+        return Response(serializer.data, status=HTTP_201_CREATED)
+
+    @subscribe.mapping.delete
+    def delete_subscribe(self, request, **kwargs):
+        author = get_object_or_404(User, id=self.kwargs.get("id"))
+        if not Subscribe.objects.filter(
+            user=request.user, author=author
+        ).exists():
+            return Response(
+                {"errors": "Данная подписка не существует"},
+                status=HTTP_400_BAD_REQUEST,
+            )
+        Subscribe.objects.filter(user=request.user, author=author).delete()
+        return Response(status=HTTP_204_NO_CONTENT)
